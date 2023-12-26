3baad39fa8bd5327b6a67e0fa6697c8cabb23f84
Orel Ashush <orel.asc11@gmail.com>
2023-12-26 20:26:21 +0200

    Write java doc for Point and Ray

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,27 +1,61 @@
 package primitives;
 import java.lang.Math;
 
+/**
+ * The Point class represents a point in three-dimensional space and provides methods for performing
+ * operations on points.
+ */
 public class Point {
     protected Double3 cords;
 
+    // The `public Point(double x,double y,double z)` constructor is creating a new `Point` object with the
+    // specified coordinates `(x, y, z)`. It initializes the `cords` variable with a new `Double3` object
+    // that holds the coordinates `(x, y, z)`.
     public Point(double x,double y,double z){
         cords = new Double3(x,y,z);
     }
 
-    protected Point(Double3 cord){//check if works
+    // The `protected Point(Double3 cord)` constructor is a protected constructor that creates a new
+    // `Point` object with the specified `Double3` object as its coordinates. It is used to create a new
+    // `Point` object from an existing `Double3` object. The `cords` variable is initialized with the
+    // `Double3` object passed as the parameter.
+    protected Point(Double3 cord){
         cords = cord;
     }
 
+    // The line `public static final Point ZERO = new Point(0, 0, 0);` is creating a constant variable
+    // named `ZERO` of type `Point`. This constant represents a point in three-dimensional space with coordinates (0, 0, 0).
     public static final Point ZERO = new Point(0, 0, 0);
 
+    /**
+     * The function subtracts the coordinates of a given point from the coordinates of the current point
+     * and returns a new vector.
+     * 
+     * @param ourPoint The parameter `ourPoint` is of type `Point`.
+     * @return A new Vector object is being returned.
+     */
     public Vector subtract(Point ourPoint){
         return new Vector(this.cords.subtract(ourPoint.cords));
     }
 
+    /**
+     * The add function takes a Vector as input and returns a new Point object with the coordinates of the
+     * sum of the input Vector and the current Point object.
+     * 
+     * @param v1 A Vector object that represents the vector to be added to the current Point object.
+     * @return The method is returning a new Point object.
+     */
     public Point add(Vector v1) {
         return new Point(v1.cords.add(this.cords));
     }
 
+    /**
+     * The function calculates the squared distance between two points in three-dimensional space.
+     * 
+     * @param thatPoint thatPoint is an instance of the Point class, representing another point in
+     * three-dimensional space.
+     * @return The method is returning the squared distance between the current point and the given point.
+     */
     public double distanceSquared(Point thatPoint){
         return(((this.cords.d1-thatPoint.cords.d1)*(this.cords.d1-thatPoint.cords.d1))+
                 ((this.cords.d2-thatPoint.cords.d2)*(this.cords.d2-thatPoint.cords.d2))+
@@ -29,10 +63,25 @@ public class Point {
         );
     }
 
+    /**
+     * The function calculates the distance between two points using the distance formula.
+     * 
+     * @param thatPoint The parameter "thatPoint" is of type "Point". It represents another point in a
+     * two-dimensional space.
+     * @return The method is returning the distance between the current point and the specified point.
+     */
     public double distance(Point thatPoint){
         return Math.sqrt(this.distanceSquared(thatPoint));
     }
 
+    /**
+     * The function checks if two Point objects are equal by comparing their coordinates.
+     * 
+     * @param Check The parameter "Check" is an Object, which means it can accept any type of object as an
+     * argument. In this case, it is being casted to a Point object inside the method.
+     * @return The method is returning a boolean value, which indicates whether the two objects being
+     * compared are equal or not.
+     */
     @Override
     public boolean equals(Object Check){
         Point CheckIfEqual=(Point) Check;
--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,15 +1,28 @@
 package primitives;
 
+/**
+ * The `Ray` class represents a ray in three-dimensional space, with a head point and a direction
+ * vector.
+ */
 public class Ray {
 
     private Point head;
     private Vector direction;
-    
+        
+    // The code snippet `public Ray(Point Head, Vector Direction) { head = Head; direction =
+    // Direction.normalize(); }` is the constructor of the `Ray` class.
     public Ray(Point Head, Vector Direction) {
         head = Head;
         direction = Direction.normalize();
     }
 
+    /**
+     * The equals() function checks if two Ray objects have the same head and direction coordinates.
+     * 
+     * @param obj The `obj` parameter is the object that we are comparing with the current object for
+     * equality.
+     * @return The method is returning a boolean value.
+     */
     @Override
     public boolean equals(Object obj) {
         return ((obj instanceof Ray other)
@@ -17,6 +30,11 @@ public class Ray {
         && this.direction.cords.equals(other.direction.cords));
     }
 
+    /**
+     * The toString() function returns a string representation of the head and direction coordinates.
+     * 
+     * @return The method is returning a string representation of the head and direction coordinates.
+     */
     @Override
     public String toString()  {
         return "head: " +  head.cords +  "\ndirection: " + direction.cords;
