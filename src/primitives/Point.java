f02bcddb82e814cb2b4603977759acea146b3210
yehoste <yehoshuaste@gmail.com>
2023-12-26 11:50:23 +0200

    change point

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,7 +1,7 @@
 package primitives;
 
 public class Point {
-
+    protected Double3 cords;
     /*#xyz: Double3
 +equals (Object): boolean
 +toString(): String
@@ -9,4 +9,34 @@ public class Point {
 +subtract (Point): Vector
 +distance(Point): double
 +distanceSquared (Point): double*/
+
+    public Point(double x,double y,double z){
+        cords = new Double3(x,y,z);
+    }
+
+    public Point(Double3 cord){//check if works
+        cords= cord;
+    }
+
+
+    public Vector subtract(Point ourPoint){
+        Vector newVector= new Vector(cords);
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public boolean equals(Object Check){
+        Point CheckIfEqual=(Point) Check;
+        return ((CheckIfEqual.cords.d1==this.cords.d1) &&
+                (CheckIfEqual.cords.d2==this.cords.d2) &&
+                (CheckIfEqual.cords.d3==this.cords.d3));
+    }
+
+
 }
