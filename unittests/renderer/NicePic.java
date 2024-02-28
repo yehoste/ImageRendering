bfd60bdbc9e8ab098a58c805c52a9165ea13b7de
Orel Ashush <orel.asc11@gmail.com>
2024-02-28 19:41:49 +0200

    end of minip 1

--- unittests/renderer/NicePic.java
+++ unittests/renderer/NicePic.java
@@ -24,7 +24,7 @@ public class NicePic {
 
     private final Scene scene = new Scene("GlossysurfacesAndBlurryGlassTests");
 
-    private final Camera.Builder cameraBuilder     = Camera.getBuilder().setRayTracer(new SimpleRayTracer(scene));
+    private final Camera.Builder cameraBuilder = Camera.getBuilder().setRayTracer(new SimpleRayTracer(scene));
 
     @Test
     public void MyPicture() throws CloneNotSupportedException {
@@ -119,7 +119,7 @@ public class NicePic {
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setkT(0.6)),
 
                 new Sphere(new Point(-25, 185, 120), 30d).setEmission(new Color(pink))
-                        .setMaterial(new Material().setKd(0.001).setKs(0.001).setShininess(30).setkT(0.2)),
+                        .setMaterial(new Material().setKd(0.001).setKs(0.001).setBlurriness(9).setShininess(30).setkT(0.2)),
 
                 new Sphere(new Point(-300, 600, 300), 30d).setEmission(new Color(YELLOW))
                         .setMaterial(new Material().setKd(0.1).setKs(0.1).setShininess(30).setkT(0.7)),
@@ -127,12 +127,12 @@ public class NicePic {
                 new Triangle(new Point(-250, 400, 0), new Point(-400, 100, 0),
                         new Point(-250, 400, 200))
                         .setEmission(new Color(20,20,20))
-                        .setMaterial(new Material().setkR(new Double3(0.5, 0, 0.4))),
+                        .setMaterial(new Material().setkR(1).setGlossiness(3)),
 
                 new Triangle(new Point(-400,100,200), new Point(-400, 100, 0),
                         new Point(-250, 400, 200))
                         .setEmission(new Color(20,20,20))
-                        .setMaterial(new Material().setkR(new Double3(0.5, 0, 0.4))),
+                        .setMaterial(new Material().setkR(1)),
 
                 new Plane(new Point(-1000,0,0), new Point(0,1000,0), new Point(-0,1000,1000))
                 .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
@@ -148,7 +148,7 @@ public class NicePic {
 
         cameraBuilder.setLocation(new Point(1200, -1100, 150)).setVpDistance(1000)
                 .setVpSize(500, 500)
-                .setImageWriter(new ImageWriter("MyPictureSSSS", 1000, 1000))
+                .setImageWriter(new ImageWriter("MyPictureGB", 1000, 1000))
                 .build()
                 .renderImage()
                 .writeToImage();
