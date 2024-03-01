f604f9db65971a736995c3a56bd7260ba31365a8
yehoste <yehoshuaste@gmail.com>
2024-03-01 14:53:17 +0200

    change test for minip2

--- unittests/renderer/NicePic.java
+++ unittests/renderer/NicePic.java
@@ -49,12 +49,7 @@ public class NicePic {
                         .setEmission(new Color(RED)) 
         );
 
-
-        scene.geometries.add(roof,
-                new Plane(new Point(1,1,0), new Point(1,0,0), new Point(0,1,0))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
-                        .setEmission(new Color(BLUE)),
-
+        Geometries house= new Geometries(
                 new Triangle(new Point(0,100,0), new Point(-50,100,0), new Point(-50,100,50))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
                         .setEmission(new Color(150,75,0)),
@@ -85,8 +80,22 @@ public class NicePic {
 
                 new Triangle(new Point(0,100,0), new Point(0,100,50), new Point(0,150,50))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
-                        .setEmission(new Color(150,75,0)),
+                        .setEmission(new Color(150,75,0))
+        );
 
+        Geometries miror= new Geometries(
+                new Triangle(new Point(-250, 400, 0), new Point(-400, 100, 0),
+                        new Point(-250, 400, 200))
+                        .setEmission(new Color(20,20,20))
+                        .setMaterial(new Material().setkR(1)),
+
+                new Triangle(new Point(-400,100,200), new Point(-400, 100, 0),
+                        new Point(-250, 400, 200))
+                        .setEmission(new Color(20,20,20))
+                        .setMaterial(new Material().setkR(1))
+        );
+
+        Geometries piller= new Geometries(
                 new Triangle(new Point(-10,170,0), new Point(-40,170,0), new Point(-10,170,120))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
                         .setEmission(new Color(BLACK)),
@@ -117,33 +126,28 @@ public class NicePic {
 
                 new Triangle(new Point(-40,170,120), new Point(-40,170,0), new Point(-40,200,120))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
-                        .setEmission(new Color(BLACK)),
+                        .setEmission(new Color(BLACK))
+        );
 
+        Geometries halfSphere= new Geometries(
                 new Sphere(new Point(-25, 185, 120),15d).setEmission(new Color(RED.brighter()))
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setkT(0.6)),
-
                 new Sphere(new Point(-25, 185, 120), 30d).setEmission(new Color(pink))
-                        .setMaterial(new Material().setKd(0.001).setKs(0.001).setShininess(30).setkT(0.2)),
+                        .setMaterial(new Material().setKd(0.001).setKs(0.001).setShininess(30).setkT(0.2))
+        );
+
+
+        scene.geometries.add(roof,house,miror,piller,halfSphere,
+                new Plane(new Point(1,1,0), new Point(1,0,0), new Point(0,1,0))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                        .setEmission(new Color(BLUE)),
 
                 new Sphere(new Point(-300, 600, 300), 30d).setEmission(new Color(YELLOW))
                         .setMaterial(new Material().setKd(0.1).setKs(0.1).setShininess(30).setkT(0.7)),
 
-                new Triangle(new Point(-250, 400, 0), new Point(-400, 100, 0),
-                        new Point(-250, 400, 200))
-                        .setEmission(new Color(20,20,20))
-                        .setMaterial(new Material().setkR(1)),
-
-                new Triangle(new Point(-400,100,200), new Point(-400, 100, 0),
-                        new Point(-250, 400, 200))
-                        .setEmission(new Color(20,20,20))
-                        .setMaterial(new Material().setkR(1)),
-
                 new Plane(new Point(-1000,0,0), new Point(0,1000,0), new Point(-0,1000,1000))
                 .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
                 .setEmission(new Color(0,255,255)));
-
-
-
 
         scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.03));
         
